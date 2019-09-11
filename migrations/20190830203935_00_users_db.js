d3f622151003850690bd736689029329fe5bb686
davideastmond <davebuck1981@gmail.com>
2019-09-10 21:01:47 -0400

    tab spacing issues

--- migrations/20190830203935_00_users_db.js
+++ migrations/20190830203935_00_users_db.js
@@ -1,44 +1,44 @@
 
 exports.up = function(knex) {
-	return Promise.all([
-		knex.schema.createTable('user', (table) => {
-			table.increments('id').primary();
-			table.string('email').unique().notNullable();
-			table.string('password');
-			table.boolean('is_registered').defaultTo(true);
-			table.boolean('has_chosen_topics').defaultTo(true);
-			table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-			table.timestamp('last_login').defaultTo(knex.fn.now());
-		}),
-		knex.schema.createTable('topic', (table) => {
-			table.increments('id').primary();
-			table.string('name').unique().notNullable();
-			table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-		}),
-		knex.schema.createTable('user_topic', (table) => {
-			table.increments('id').primary();
-			
-			table.integer('user_id');
-			table.integer('topic_id');
+  return Promise.all([
+    knex.schema.createTable('user', (table) => {
+      table.increments('id').primary();
+      table.string('email').unique().notNullable();
+      table.string('password');
+      table.boolean('is_registered').defaultTo(true);
+      table.boolean('has_chosen_topics').defaultTo(true);
+      table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+      table.timestamp('last_login').defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTable('topic', (table) => {
+      table.increments('id').primary();
+      table.string('name').unique().notNullable();
+      table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTable('user_topic', (table) => {
+      table.increments('id').primary();
+      
+      table.integer('user_id');
+      table.integer('topic_id');
 
-			table.foreign('user_id').references('user.id');
-			table.foreign('topic_id').references('topic.id');
-		})
-	])
-	.then(()=> {
-		console.log("Tables created.");
-	});
+      table.foreign('user_id').references('user.id');
+      table.foreign('topic_id').references('topic.id');
+    })
+  ])
+  .then(()=> {
+    console.log("Tables created.");
+  });
 };
 
 exports.down = function(knex) {
-	return Promise.all([
-		knex.schema.dropTable('user_topic'),
-		knex.schema.dropTable('user'),
-		knex.schema.dropTable('topic'),
-		
-	])
-	.then(()=> {
-		console.log("Tables dropped.");
-	});
+  return Promise.all([
+    knex.schema.dropTable('user_topic'),
+    knex.schema.dropTable('user'),
+    knex.schema.dropTable('topic'),
+    
+  ])
+  .then(()=> {
+    console.log("Tables dropped.");
+  });
   
 };
--- seeds/first_seed.js
+++ seeds/first_seed.js
@@ -1,22 +1,8 @@
 const functionHelpers = require('../helper');
-
-// exports.seed = function(knex) {
-//   // Deletes ALL existing entries
-//   return knex('table_name').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('table_name').insert([
-//         {id: 1, colName: 'rowValue1'},
-//         {id: 2, colName: 'rowValue2'},
-//         {id: 3, colName: 'rowValue3'}
-//       ]);
-//     });
-// };
-
 exports.seed = function(knex) {
   // Delete ALL Existing entries from each table
   return Promise.all([
-		knex('user_topic').del(),
+    knex('user_topic').del(),
     knex('user').del(),
     knex('topic').del()
   ])
@@ -30,40 +16,40 @@ exports.seed = function(knex) {
       return Promise.all([
         knex('topic').insert({ id: 1, name: 'politics'}),
         knex('topic').insert({ id: 2, name: 'sports'}),
-				knex('topic').insert({ id: 3, name: 'science'}),
-				knex('topic').insert({ id: 4, name: 'reading'}),
-				knex('topic').insert({ id: 5, name: 'education'}),
-				knex('topic').insert({ id: 6, name: 'technology'}),
-				knex('topic').insert({ id: 7, name: 'space'}),
-				knex('topic').insert({ id: 8, name: 'music'}),
-				knex('topic').insert({ id: 9, name: 'broadway'}),
-				knex('topic').insert({ id: 10, name: 'law'})
+        knex('topic').insert({ id: 3, name: 'science'}),
+        knex('topic').insert({ id: 4, name: 'reading'}),
+        knex('topic').insert({ id: 5, name: 'education'}),
+        knex('topic').insert({ id: 6, name: 'technology'}),
+        knex('topic').insert({ id: 7, name: 'space'}),
+        knex('topic').insert({ id: 8, name: 'music'}),
+        knex('topic').insert({ id: 9, name: 'broadway'}),
+        knex('topic').insert({ id: 10, name: 'law'})
       ])
       .then(()=> {
         return Promise.all([
           knex('user_topic').insert({ id: 1, user_id: 1, topic_id: 1}),
           knex('user_topic').insert({ id: 2, user_id: 1, topic_id: 2 }),
-					knex('user_topic').insert({ id: 3, user_id: 1, topic_id: 3 }),
-					knex('user_topic').insert({ id: 4, user_id: 1, topic_id: 4 }),
-					knex('user_topic').insert({ id: 5, user_id: 1, topic_id: 5 }),
-					knex('user_topic').insert({ id: 6, user_id: 1, topic_id: 6 }),
-					knex('user_topic').insert({ id: 7, user_id: 1, topic_id: 7 }),
-					knex('user_topic').insert({ id: 8, user_id: 1, topic_id: 8 }),
-					knex('user_topic').insert({ id: 9, user_id: 1, topic_id: 9 }),
-					knex('user_topic').insert({ id: 10, user_id: 1, topic_id: 10 }),
+          knex('user_topic').insert({ id: 3, user_id: 1, topic_id: 3 }),
+          knex('user_topic').insert({ id: 4, user_id: 1, topic_id: 4 }),
+          knex('user_topic').insert({ id: 5, user_id: 1, topic_id: 5 }),
+          knex('user_topic').insert({ id: 6, user_id: 1, topic_id: 6 }),
+          knex('user_topic').insert({ id: 7, user_id: 1, topic_id: 7 }),
+          knex('user_topic').insert({ id: 8, user_id: 1, topic_id: 8 }),
+          knex('user_topic').insert({ id: 9, user_id: 1, topic_id: 9 }),
+          knex('user_topic').insert({ id: 10, user_id: 1, topic_id: 10 }),
 
           knex('user_topic').insert({ id: 11, user_id: 2, topic_id: 1 }),
           knex('user_topic').insert({ id: 12, user_id: 2, topic_id: 2 }),
 
           knex('user_topic').insert({ id: 13, user_id: 3, topic_id: 9 })
         ]);
-			})
-			.then(() => {
-				return Promise.all([
-					knex.raw('ALTER SEQUENCE topic_id_seq RESTART WITH 11;'),
-					knex.raw('ALTER SEQUENCE user_topic_id_seq RESTART WITH 14;')
-				]);
-			});
+      })
+      .then(() => {
+        return Promise.all([
+          knex.raw('ALTER SEQUENCE topic_id_seq RESTART WITH 11;'),
+          knex.raw('ALTER SEQUENCE user_topic_id_seq RESTART WITH 14;')
+        ]);
+      });
     });
   });
 };
