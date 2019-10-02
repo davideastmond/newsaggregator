bb38286c4be8a62f7de537c762bdec37e8d38405
davideastmond <davebuck1981@gmail.com>
2019-10-02 16:43:47 -0400

    code refactor / clean up

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-			host: process.env.DB_HOST,
-					user: process.env.DB_USER,
-					password: process.env.DB_USER_PASSWORD,
-					database: process.env.DB_NAME
-		}
+      host: process.env.DB_HOST,
+				user: process.env.DB_USER,
+				password: process.env.DB_USER_PASSWORD,
+				database: process.env.DB_NAME
+    }
   },
 };
