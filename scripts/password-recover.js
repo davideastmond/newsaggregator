6fafa62cfbd0a7ff189fc3aadf4792f792ebb3eb
davideastmond <davebuck1981@gmail.com>
2020-10-19 08:46:47 -0400

    front-end tweaks

--- scripts/password-recover.js
+++ scripts/password-recover.js
@@ -9,7 +9,7 @@ $(() => {
     const passwordText2 = document.getElementById('change-password-box-two').value;
     $('#recover-password-submit-button').attr('disabled', true);
     if (passwordText1 !== passwordText2) {
-      $("#change-password-error-message").css('display', 'block').text('Ensure your passwords match');
+      $("#change-password-error-message").css({ 'display': 'block', 'color': 'red' }).text('Ensure your passwords match');
       return;
     }
 
@@ -24,12 +24,20 @@ $(() => {
       console.log(res)
       if (res.status === "OK") {
         $("#change-password-success-message").css('display', 'block').text("Password was changed successfully!")
+        $(".update-form-area").css('display', 'none');
         $(".login-headlines-back-link").css('display', 'block');
         $('#recover-password-submit-button').css('display', 'none');
       } else {
         $("#change-password-error-message").css({ 'display': 'block', 'color': 'red' }).text('There was a problem updating the password.')
       }
     });
+  });
+
+  $("#change-password-box-one").click((e) => {
+    clearValidationErrorMessage();
+  });
+  $("#change-password-box-two").click((e) => {
+    clearValidationErrorMessage();
   })
 })
 
