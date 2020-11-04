757234eb55bc1d7a0131f82977b48df4376a4554
davideastmond <davebuck1981@gmail.com>
2020-11-03 19:17:27 -0500

    continued work: toggle for radio buttons

--- scripts/sources.js
+++ scripts/sources.js
@@ -1,24 +1,55 @@
-import { checkBoxSource } from "./partials/source-checkbox-item.js"
-var masterList;
+import { checkBoxSource } from './partials/source-checkbox-item.js';
+let masterList;
 $(async () => {
   // Test get sources
-  const { data } = await axios.get('/user/user/sources')
-  const { allSources, userSources } = data
-  console.log(allSources)
-  console.log(userSources)
+  const { data } = await axios.get('/user/user/sources');
+  const { allSources, userSources } = data;
+  console.log(allSources);
+  console.log(userSources);
 
-  loadNewsSourceList(allSources.sources)
-  masterList = allSources.sources
+  loadNewsSourceList(allSources.sources);
+  masterList = allSources.sources;
 
   $('#sources-search-textbox-filter').on('keyup', () => {
     const filterString = $('#sources-search-textbox-filter').val().toLowerCase();
     const filteredSourcesList = masterList.filter((source) => {
-      return source.name.toLowerCase().includes(filterString)
-    })
-    loadNewsSourceList(filteredSourcesList)
-  })
-})
+      return source.name.toLowerCase().includes(filterString);
+    });
+    loadNewsSourceList(filteredSourcesList);
+  });
 
+  $('#source-option-global').on('click', (e) => {
+    $('#source-option-global').prop('checked', true);
+    $('.option-global').addClass('active');
+    $('.option-off').removeClass('active');
+    $('.option-topic').removeClass('active');
+    $('.search-text-container').removeClass(['hide', 'half']);
+    $('.sources-list-back-ground').removeClass(['hide', 'half']);
+  });
+
+  $('#source-option-off').on('click', (e) => {
+    $('#source-option-off').prop('checked', true);
+    $('.option-off').addClass('active');
+    $('.option-global').removeClass('active');
+    $('.option-topic').removeClass('active');
+    $('.search-text-container').addClass('hide');
+    $('.sources-list-back-ground').addClass('hide');
+  });
+
+  $('#source-option-topic').on('click', (e) => {
+    $('#source-option-topic').prop('checked', true);
+    $('.option-global').removeClass('active');
+    $('.option-off').removeClass('active');
+    $('.option-topic').addClass('active');
+    $('.search-text-container').addClass('half').removeClass('hide');
+    $('.sources-list-back-ground').removeClass('hide').addClass('half');
+  });
+});
+
+/**
+ *
+ * @param {[{}]} sourceArray
+ */
 function loadNewsSourceList(sourceArray) {
   $('.ul-sources-list').empty();
   $('.ul-sources-list').html(sourceArray.map((source) => {
@@ -29,4 +60,4 @@ function loadNewsSourceList(sourceArray) {
       cbChecked: null,
     };
   }).map(checkBoxSource).join(''));
-} 
+}
